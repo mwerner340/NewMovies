8d26abd574915d7e97580cba045a4cbea729b1e9
mwerner340 <mikewerner@att.net>
2014-07-01 16:56:18 -0400

    2

---
+++ MvcMovieProject/MvcMovieProject/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcMovieProject.Models
+{
+    public class Movie
+    {
+            public int ID { get; set; }
+            public string Title { get; set; }
+            public DateTime ReleaseDate { get; set; }
+            public string Genere { get; set; }
+            public decimal Price { get; set; }
+        }
+        public class MovieDBContext : DbContext
+        {
+            public DbSet<Movie> Movies { get; set; }
+
+        }
+    }
+
+  
---
+++ MvcMovieProject/MvcMovieProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcMovieProject.Startup))]
+namespace MvcMovieProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
